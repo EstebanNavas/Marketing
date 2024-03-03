42f380a1b1a432c5b32f20a6de62ba429e062ddd
EstebanNavas <arcoesteb@live.com>
2024-03-02 20:02:35 -0500

    Se crea reporte Recaudos Periodo

---
+++ src/main/java/com/marketing/Projection/TblPagosDTO.java
@@ -0,0 +1,28 @@
+package com.marketing.Projection;
+
+public interface TblPagosDTO {
+	
+	String getIdCliente();
+	String getNombreTercero();
+	Integer getIdLocal();
+	Integer getIdTipoOrden();
+	Integer getIdRecibo();
+	Integer getIndicador();
+	String getFechaPago();
+	Double getvrPago();
+	Double getVrRteFuente();
+	Double getVrDescuento();
+	Double getVrRteIva();
+	Double getIdDcto();
+	String getIdDctoNitCC();
+	Double getIdPlanilla();
+	Double getVrRteIca();
+	String getAliasUsuario();
+	Integer getIdPeriodo();
+	Integer getIdMedio();
+	String getNombreMedio();
+	String getNombreRuta();
+	String getNombreEstracto();
+	String getNitCC();
+
+}
--- src/main/java/com/marketing/Service/dbaquamovil/TblPagosService.java
+++ src/main/java/com/marketing/Service/dbaquamovil/TblPagosService.java
@@ -1,8 +1,11 @@
 package com.marketing.Service.dbaquamovil;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marketing.Projection.TblPagosDTO;
 import com.marketing.Repository.dbaquamovil.TblPagosRepo;
 
 @Service
@@ -10,4 +13,27 @@ public class TblPagosService {
 
 	@Autowired
 	TblPagosRepo tblPagosRepo;
+	
+	public List<TblPagosDTO> listaRepRecaudoPeriodo(int idLocal, int idPeriodo){
+		
+		List<TblPagosDTO> lista = tblPagosRepo.listaRepRecaudoPeriodo(idLocal, idPeriodo);
+		
+		return lista;
+	}
+	
+	
+	public  List<TblPagosDTO> listaRepRecaudoPeriodoRuta(int idLocal, int idPeriodo, int idRuta){
+		
+		List<TblPagosDTO> lista = tblPagosRepo.listaRepRecaudoPeriodoRuta(idLocal, idPeriodo, idRuta);
+		
+		return lista;
+	}
+	
+	
+	public List<TblPagosDTO> listaRepRecaudoPeriodoFecha(int idLocal, int idPeriodo, String fecha){
+		
+		List<TblPagosDTO> lista = tblPagosRepo.listaRepRecaudoPeriodoFecha(idLocal, idPeriodo, fecha);
+		
+		return lista;
+	}
 }
