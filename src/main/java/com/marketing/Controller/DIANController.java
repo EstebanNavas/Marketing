13fe16e11f1172828c03b64ad3db6ea327027db6
EstebanNavas <arcoesteb@live.com>
2025-06-02 23:23:56 -0500

    Se agrega apiSPD en el switch

--- src/main/java/com/marketing/Controller/DIANController.java
+++ src/main/java/com/marketing/Controller/DIANController.java
@@ -205,6 +205,10 @@ public class DIANController {
 	    case 320:
 	    	ApiFE = "sudo nohup ./apisoenacspd.sh";
 	        break;
+	        
+	    case 1300:
+	    	ApiFE = "apiSPD.sh";
+	        break;
 	    
 	    default:
 	    	ApiFE = "";
@@ -448,6 +452,10 @@ public class DIANController {
 	        
 	    case 320:
 	    	ApiFE = "sudo nohup ./apisoenacspd.sh";
+	        break;
+	        
+	    case 1300:
+	    	ApiFE = "apiSPD.sh";
 	        break;
 	    
 	    default:
